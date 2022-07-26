a8e585a67bd424eb0f32b9c152b38b353f22923f
Nathna <nathangabrielvf@gmail.com>
2022-07-26 15:15:04 -0300

    Inserido no FIREBASEEEEEEEEEEEE!!!!!!!!

--- src/pages/Home/props.tsx
+++ src/pages/Home/props.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
- import { Button, TextInput, View } from 'react-native';
- import { Formik } from 'formik';
- import { InputPass, Inputemail } from './styles';
+// import React from 'react';
+//  import { Button, TextInput, View } from 'react-native';
+//  import { Formik } from 'formik';
+//  import { InputPass, Inputemail } from './styles';
 
-//  export const Conections = props => (
-export const Conections = props => (
+// //  export const Conections = props => (
+// export const Conections = props => (
 
-  <Formik
-    initialValues={{Inputemail: '', InputPass: '' }}
-    onSubmit={values => console.log(values)}
-  >
-    {({ handleChange, handleBlur, handleSubmit, values }) => (
-      <View>
-          <Inputemail
-          onChangeText={handleChange('Inputemail')}
-          onBlur={handleBlur('Inputemail')}
-          value={values.Inputemail}
-        />
-          <InputPass
-          onChangeText={handleChange('InputPass')}
-          onBlur={handleBlur('InputPass')}
-          value={values.InputPass}
-        />
-        <Button onPress={() => handleSubmit()} title="Submit" />
-      </View>
-    )}
-  </Formik>
-);
+//   <Formik
+//     initialValues={{Inputemail: '', InputPass: '' }}
+//     onSubmit={values => console.log(values)}
+//   >
+//     {({ handleChange, handleBlur, handleSubmit, values }) => (
+//       <View>
+//           <Inputemail
+//           onChangeText={handleChange('Inputemail')}
+//           onBlur={handleBlur('Inputemail')}
+//           value={values.Inputemail}
+//         />
+//           <InputPass
+//           onChangeText={handleChange('InputPass')}
+//           onBlur={handleBlur('InputPass')}
+//           value={values.InputPass}
+//         />
+//         <Button onPress={() => handleSubmit()} title="Submit" />
+//       </View>
+//     )}
+//   </Formik>
+// );
